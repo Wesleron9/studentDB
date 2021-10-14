bfee8a568b4be6930e7bab969d4433c894927cef
Wesleron9 <wesleron99@gmail.com>
2021-10-14 23:59:57 +0300

    Update script.js

--- DormService/modules/auth/script.js
+++ DormService/modules/auth/script.js
@@ -6,9 +6,10 @@ document.querySelector('#signin').addEventListener('click', () => {
     return
   }
 
+  let body = JSON.stringify({login:login, password:password})
   const xhr = new XMLHttpRequest()
   xhr.open('POST', '../../test.php', false)
-  xhr.send({login: '1234',password: '5432'})
+  xhr.send(body)
   
   if (xhr.status != 200) {
     alert(xhr.status + ': ' + xhr.statusText ); 
