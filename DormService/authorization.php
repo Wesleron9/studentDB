40dc145990911a302744203a95500df57b730d77
Dewalt1317 <roman.sidorenko2000@icloud.com>
2021-10-16 03:49:30 +0300

    Update authorization.php

--- DormService/authorization.php
+++ DormService/authorization.php
@@ -3,24 +3,24 @@
     FILTER_SANITIZE_STRING);
     $passwd = filter_var(trim($_POST['password']),
     FILTER_SANITIZE_STRING);
- //   $name = filter_var(trim($_POST['name']),
- //   FILTER_SANITIZE_STRING);
-    $name = "test"
+//   $name = filter_var(trim($_POST['name']),
+//   FILTER_SANITIZE_STRING);
+    $name = "test";
     if(mb_strlen($login) <5 || mb_strlen($login) > 90) {
-        echo "Недопустимая длина логина (От 5 до 90 символов)"
+       echo "Недопустимая длина логина (От 5 до 90 символов)";
+       exit();
+    }  else if (mb_strlen($passwd) <8 || mb_strlen($login) > 32) {
+        echo "Недопустимая длина пароля (от 8 до 32 символов)";
         exit();
-        }
-        else if (mb_strlen($passwd) <8 || mb_strlen($login) > 32) {
-    echo "Недопустимая длина пароля (от 8 до 32 символов)"
-            exit();
-
+    }   else if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+        echo "Недопустимая длина ФИО (от 3 до 50 символов)";
+        exit();
     }
-        else if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
-echo "Недопустимая длина ФИО (от 3 до 50 символов)"
-            exit();
- }
-    $mysql = new mysqli('127.0.0.1:3306', 'root', 'rs2076571', 'dorm_db');
-    $mysql->query("INSERT INTO `users` (`login`, `pass`, `name`) VALUES ('$login', '$passwd', '$name')");
-    $mysql->close()
-echo $_POST['login']
+    $mysql = new mysqli('127.0.0.1', 'root', 'rs2076571', 'dorm_db');
+    $mysql->query("INSERT INTO `users` (`login`, `pass`, `name`)
+    VALUES ('$login', '$passwd', '$name')");
+    $mysql->close();
+//echo $_POST['login'];
+//$login = $_POST['login'];
+//echo $login;
 ?> 
