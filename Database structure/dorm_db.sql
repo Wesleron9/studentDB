d7c3fe6591e0818a633df776f94567dc05e23ee2
Dewalt1317 <roman.sidorenko2000@icloud.com>
2021-10-16 18:34:26 +0300

    Update dorm_db.sql

--- Database structure/dorm_db.sql
+++ Database structure/dorm_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 14 2021 г., 19:38
+-- Время создания: Окт 16 2021 г., 18:32
 -- Версия сервера: 5.7.33
 -- Версия PHP: 7.1.33
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- База данных: `dorm_db`
 --
+CREATE DATABASE IF NOT EXISTS `dorm_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `dorm_db`;
 
 -- --------------------------------------------------------
 
@@ -31,8 +33,18 @@ CREATE TABLE `users` (
   `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(50) NOT NULL,
   `login` varchar(100) NOT NULL,
-  `pass` varchar(32) NOT NULL
+  `pass` varchar(32) NOT NULL,
+  `role` int(3) DEFAULT NULL,
+  `Confirmed` tinyint(1) DEFAULT NULL,
+  `Email` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `login`, `pass`, `role`, `Confirmed`, `Email`) VALUES
+(10, 'Роман Сидоренко', 'sidorenri', '49e0ac7a251e79752178ad9fc4167336', NULL, NULL, NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -52,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
