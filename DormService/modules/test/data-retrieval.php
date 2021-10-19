8311227953059e3a7b254399fa4659265fc26445
Dewalt1317 <roman.sidorenko2000@icloud.com>
2021-10-19 07:12:55 +0300

    ещё раз поправил

--- DormService/modules/test/data-retrieval.php
+++ DormService/modules/test/data-retrieval.php
@@ -7,7 +7,7 @@ if ($result->num_rows > 0){
     while ($row = $result->fetch_assoc()){
         $arr[$num][] =array($row['name'], $row['id']);
         //echo "КОД:  ".$arr[$user][0][1]."  ---  ФИО: ".$arr[$user][0][0].'<br>';
-        $user++;
+        //$user++;
         $num ++;
        //echo 'ФИО: '.$row['name'].'<br>';
     }
@@ -22,5 +22,5 @@ $json = json_encode($arr, JSON_UNESCAPED_UNICODE);
 //echo '<a href="http://qrcoder.ru" target="_blank"><img src="http://qrcoder.ru/code/?http%3A%2F%2F192.168.1.116%2Fmodules%2Fstudents%2Fdata-retrieval.php&4&0" width="164" height="164" border="0" title="QR код"></a>'
 ////$user = 1;
 ////echo $arr[$user][0][1];
-print_r($json);
+//print_r($json);
 ?>
