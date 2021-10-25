32ae9b9020d8a68dc279e3db98b8e9570bc80055
Wesleron9 <wesleron99@gmail.com>
2021-10-25 19:50:41 +0300

    Format scripts

--- DormService/authorization.php
+++ DormService/authorization.php
@@ -1,54 +1,66 @@
 <?php
-//Объявляем приём данных
+//Объявляем на какие данные расчитан этот скрипт
 header("Content-Type: application/json");
+
 //Стартуем сессию
 session_start();
+
 //Принимаем данные с фронта
 $data = json_decode(file_get_contents("php://input"));
-$login = filter_var(trim($data->login),
-    FILTER_SANITIZE_STRING);
-$passwd = filter_var(trim($data->password),
-    FILTER_SANITIZE_STRING);
+$login = filter_var(
+    trim($data->login),
+    FILTER_SANITIZE_STRING
+);
+$passwd = filter_var(
+    trim($data->password),
+    FILTER_SANITIZE_STRING
+);
+
 //Валидация
-if ($login ="") {
+if ($login = "") {
     echo "Введите логин";
     exit();
-    }
-    else if ($passwd = "") {
+} else if ($passwd = "") {
     echo "Введите пароль";
-        exit();
-        }
+    exit();
+}
+
 //Добавляем соль к паролю и хешируем
-$passwd = md5($passwd."matveeva");
+$passwd = md5($passwd . "matveeva");
+
 //Подключаемся к БД
 require "connection-to-db.php";
+
 //Проверяем есть ли пользователь в таблице временных пользователей
 $result_temp = $mysql->query("SELECT `login`, `pass` FROM `temp users` WHERE  `login` = '$login' and `pass` = '$passwd'");
-$user_temp =$result_temp->fetch_assoc();
+$user_temp = $result_temp->fetch_assoc();
+
 //Проверяем есть ли пользователь в таблице основных пользователей
 $result = $mysql->query("SELECT `login`, `pass`, `name`, `role`, `key`, `way to photo` FROM `users` WHERE  `login` = '$login' and `pass` = '$passwd'");
-$user =$result->fetch_assoc();
+$user = $result->fetch_assoc();
+
 //Если есть во временных пользователях
-if(count($user_temp) <>0){
+if (count($user_temp) <> 0) {
     echo "Ожидайте подверждение вашей учетной записи";
     $mysql->close();
     exit();
-}
-//Если нет в основных пользователях
-elseif (count($user) ==0){
+} else if (count($user) == 0) { //Если нет в основных пользователях
     echo "Неверный логин или пароль";
     $mysql->close();
     exit();
 }
+
 //После умпешной авторизации добавляем логин и ключ в сессию
-    $_SESSION['user name'] = $login;
-    $_SESSION[key] = $user[key];
+$_SESSION['user name'] = $login;
+$_SESSION[key] = $user[key];
+
 //Узнаем роль пользователя
-    $role_result = $mysql->query("SELECT `role`, `role id` FROM `role` WHERE  `role id` = $user[role]");
-    $role = $role_result->fetch_assoc();
+$role_result = $mysql->query("SELECT `role`, `role id` FROM `role` WHERE  `role id` = $user[role]");
+$role = $role_result->fetch_assoc();
+
 //Закрываем соеденение с БД
-    $mysql->close();
+$mysql->close();
+
 //Создаём массив для передачи на фронт, и передаём его как json объект
-    $user = [name => $user[name], role => $role[role], photo => $user['way to photo']];
-    $json = json_encode($user, JSON_UNESCAPED_UNICODE);
-?>
+$user = [name => $user[name], role => $role[role], photo => $user['way to photo']];
+$json = json_encode($user, JSON_UNESCAPED_UNICODE);
--- DormService/registration.php
+++ DormService/registration.php
@@ -1,32 +1,43 @@
 <?php
-//Объявляем прием данных
+//Объявляем на какие данные расчитан этот скрипт
 header("Content-Type: application/json");
+
 //Принимаем данные с фронта
 $data = json_decode(file_get_contents("php://input"));
-    $login = filter_var(trim($data->login),
-    FILTER_SANITIZE_STRING);
-    $passwd = filter_var(trim($data->password),
-    FILTER_SANITIZE_STRING);
-   $name = filter_var(trim($data->name),
-   FILTER_SANITIZE_STRING);
+$login = filter_var(
+    trim($data->login),
+    FILTER_SANITIZE_STRING
+);
+$passwd = filter_var(
+    trim($data->password),
+    FILTER_SANITIZE_STRING
+);
+$name = filter_var(
+    trim($data->name),
+    FILTER_SANITIZE_STRING
+);
+
 //Валидация
-   if(mb_strlen($login) <4 || mb_strlen($login) > 90) {
-       echo "Недопустимая длина логина (От 4 до 90 символов)";
-       exit();
-    }  else if (mb_strlen($passwd) <8 || mb_strlen($login) > 32) {
-        echo "Недопустимая длина пароля (от 8 до 32 символов)";
-        exit();
-    }   else if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
-        echo "Недопустимая длина ФИО (от 3 до 50 символов)";
-        exit();
-    }
+if (mb_strlen($login) < 4 || mb_strlen($login) > 90) {
+    echo "Недопустимая длина логина (От 4 до 90 символов)";
+    exit();
+} else if (mb_strlen($passwd) < 8 || mb_strlen($login) > 32) {
+    echo "Недопустимая длина пароля (от 8 до 32 символов)";
+    exit();
+} else if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+    echo "Недопустимая длина ФИО (от 3 до 50 символов)";
+    exit();
+}
+
 //Добавляем соль к паролю и хешируем
-    $passwd = md5($passwd."matveeva");
+$passwd = md5($passwd . "matveeva");
+
 //Подключаемся к БД
 require "connection-to-db.php";
+
 //Вносим данные о регистрации в таблицу не подтвержденных ползователей
-    $mysql->query("INSERT INTO `temp users` (`login`, `pass`, `name`)
+$mysql->query("INSERT INTO `temp users` (`login`, `pass`, `name`)
     VALUES ('$login', '$passwd', '$name')");
+
 //Закрываем соеденение с БД
-   $mysql->close();
-?> 
+$mysql->close();
