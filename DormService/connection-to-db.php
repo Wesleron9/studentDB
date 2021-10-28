16847e65725faed29d4a720581bbee9d3028c5f6
Wesleron9 <wesleron99@gmail.com>
2021-10-28 09:19:43 +0300

    Fix абракадабры в БД

--- DormService/connection-to-db.php
+++ DormService/connection-to-db.php
@@ -1,4 +1,8 @@
 <?php
 //Подключаемся к БД
 $mysql = new mysqli('server71.hosting.reg.ru:3306/', 'u1466303_dormdb', '6ZFF5imM5rvWHv8', 'u1466303_dormdb');
-?>
+
+// Установка кодировки соединения
+$mysql->query("SET NAMES utf8");
+$mysql->query("SET CHARACTER SET utf8");
+$mysql->query("SET character_set_connection=utf8");
