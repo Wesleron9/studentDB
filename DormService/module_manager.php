cb0f086b2ac6a551b7a314a7c0a9f0815d68ad31
Wesleron9 <wesleron99@gmail.com>
2021-11-03 15:21:42 +0300

    Update module_manager.php

--- DormService/module_manager.php
+++ DormService/module_manager.php
@@ -7,79 +7,71 @@ header("Content-Type: application/json"); // Объявляем на какие 
 
 session_start(); // Стартуем сессию
 
-    $data = json_decode(file_get_contents("php://input")); // Принимаем данные с фронта
-    $task = filter_var(
-        trim($data->task),
-        FILTER_SANITIZE_STRING
+$data = json_decode(file_get_contents("php://input")); // Принимаем данные с фронта
+$task = filter_var(
+    trim($data->task),
+    FILTER_SANITIZE_STRING
 );
-    $module = filter_var(
-        trim($data->module),
-        FILTER_SANITIZE_STRING
+$module = filter_var(
+    trim($data->module),
+    FILTER_SANITIZE_STRING
 );
-    $function = filter_var(
-        trim($data->function),
-        FILTER_SANITIZE_STRING
+$function = filter_var(
+    trim($data->function),
+    FILTER_SANITIZE_STRING
 );
 
 $user = $_SESSION['user name']; // Проверяем кто авторизован
 
-if ($user = " "){ // Если никто не авторизован
+if ($user == " ") { // Если никто не авторизован
 
     systemMessage("Ошибка авторизации, попробуйте снова");
     $mysql->close(); // Закрываем соеденение с БД
     exit(); // Завершаем скрипт
-}
-
-elseif ($task = "menu"){ // Запрос меню
+} elseif ($task == "menu") { // Запрос меню
 
     $result = $mysql->query("SELECT `module`, `module name`, `ico`, `#` FROM `mudels-user` WHERE  `login` = '$user' ORDER BY `#`"); // Запрос к БД какие модули доступны
 
-         if ($result->num_rows > 0) { // Если запрос отдал больше 0 строк
-             while ($menu = $result->fetch_assoc()) { // Выбераем записи
-                $arr[$menu['#']] = array("menu-name" => $menu['module'], "module-name" => $menu['module name'], "wey-ico" => $menu['ico']); // Добавляем записи в массив
-             }
-             systemResponse($arr); // Отправляем маcсив на фронт
-         }
-         else{ // Если запрос не отдал ни одной строки
-            systemMessage("Не один пункт меню не доступен, обратитесь к администратору");
-         }
+    if ($result->num_rows > 0) { // Если запрос отдал больше 0 строк
+        while ($menu = $result->fetch_assoc()) { // Выбераем записи
+            $arr[$menu['#']] = array("menu-name" => $menu['module'], "module-name" => $menu['module name'], "wey-ico" => $menu['ico']); // Добавляем записи в массив
+        }
+        systemResponse($arr); // Отправляем маcсив на фронт
+    } else { // Если запрос не отдал ни одной строки
+        systemMessage("Не один пункт меню не доступен, обратитесь к администратору");
+    }
 
-         $mysql->close(); // Закрываем соеденение с БД
-         exit(); // Завершаем скрипт
-}
-elseif ($task ="module"){ // Запрос модуля
+    $mysql->close(); // Закрываем соеденение с БД
+    exit(); // Завершаем скрипт
+} elseif ($task == "module") { // Запрос модуля
 
     $result = $mysql->query("SELECT `module`FROM `mudels-user` WHERE  `login` = '$user' and `module` = '$module'"); // Проверяем доступен ли модуль пользователю
 
-    if ($result->num_rows > 0){ // Если доступен
+    if ($result->num_rows > 0) { // Если доступен
 
         $result = $mysql->query("SELECT `css`, `java`, `html`, `php` FROM `mudels` WHERE  `mudels` = '$module'"); // Находим пути до модуля
-            $wey_modules = $result->fetch_assoc(); // Выбераем записи
+        $wey_modules = $result->fetch_assoc(); // Выбераем записи
 
-                $arr = ["css" => $wey_modules['css'], "java" => $wey_modules['java'], "html" => $wey_modules['html'], "php" => $wey_modules['php']]; // Добавляем записи в массив
-                systemResponse($arr); // Отправляем маcсив на фронт
+        $arr = ["css" => $wey_modules['css'], "java" => $wey_modules['java'], "html" => $wey_modules['html'], "php" => $wey_modules['php']]; // Добавляем записи в массив
+        systemResponse($arr); // Отправляем маcсив на фронт
 
 
-    }
-    else{ // Если недоступен
+    } else { // Если недоступен
         systemMessage("Недостаточно прав для просмотра этого модуля, обратитесь к администратору");
     }
     $mysql->close(); // Закрываем соеденение с БД
     exit(); // Завершаем скрипт
-}
-elseif ($task = "function") { // Запрос доступа к функции
+} elseif ($task == "function") { // Запрос доступа к функции
     $result = $mysql->query("SELECT `function` FROM `$module` WHERE  `login` = '$user' and `function` = '$function'"); // Проверяем доступна ли функция пользователю
-    if ($result->num_rows > 0) {// Если доступна
+    if ($result->num_rows > 0) { // Если доступна
         $arr = [true];
         systemResponse($arr); // Отправляем маcсив на фронт
+    } else {
+        systemMessage("Недостаточно прав для использования этой функции, обратитесь к администратору");
     }
-else{
-    systemMessage("Недостаточно прав для использования этой функции, обратитесь к администратору");
-}
     $mysql->close(); // Закрываем соеденение с БД
     exit(); // Завершаем скрипт
-}
-else{ // Если поступила неизвестная задача
+} else { // Если поступила неизвестная задача
     systemMessage("Неизвестная задача");
     $mysql->close(); // Закрываем соеденение с БД
     exit(); // Завершаем скрипт
