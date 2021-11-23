501e968d47da4b3c0dba1d8b9432e7bd91baa26e
Dewalt1317 <roman.sidorenko2000@icloud.com>
2021-11-23 07:19:16 +0300

    Ещё раз допилил регистрацию

--- DormService/login_session.php
+++ DormService/login_session.php
@@ -1,12 +1,13 @@
 <?php
+//Стартуем сессию
 session_start();
 $login = $_SESSION['user name'];
 //Берём данные пользователя из БД
-$result = $mysql->query("SELECT `login`, `pass`, `name`, `role`, `photo` FROM `users` WHERE  `login` = '$login'");
+$result = $mysql->query("SELECT `name`, `role`, `photo` FROM `users` WHERE  `login` = '$login'");
 $user = $result->fetch_assoc();
 //Узнаем роль пользователя
 $role_result = $mysql->query("SELECT `role`, `role id` FROM `role` WHERE  `role id` = $user[role]");
-$role = $role_result->fetch_assoc()
+$role = $role_result->fetch_assoc();
 //Закрываем соеденение с БД
 $mysql->close();
 //Создаём массив для передачи на фронт, и передаём его как json объект
